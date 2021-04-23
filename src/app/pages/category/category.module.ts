344ddf3796117ed0aca76bbc17d3c7201d964437
Matondo Pedro <matondo.antonio@ideiasdinamicas.com>
2021-04-23 14:56:11 +0100

    add navbar

---
+++ src/app/pages/category/category.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CategoryRoutingModule } from './category-routing.module';
+import { CategoryListComponent } from './category-list/category-list.component';
+import { CategoryFormComponent } from './category-form/category-form.component';
+
+
+@NgModule({
+  declarations: [
+    CategoryListComponent,
+    CategoryFormComponent
+  ],
+  imports: [
+    CommonModule,
+    CategoryRoutingModule
+  ]
+})
+export class CategoryModule { }
