f35f51f0f80699fdbdc3dc29b80dc119300fb3de
Matondo Pedro <matondo.antonio@ideiasdinamicas.com>
2021-05-13 14:45:31 +0100

    delete cotegories

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CategoryModule } from './pages/category/category.module';
-
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api'
+import { inMemoryService } from './in-memory-service'
 @NgModule({
   declarations: [
     AppComponent
@@ -12,6 +13,8 @@ import { CategoryModule } from './pages/category/category.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(inMemoryService)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/category/category-list/category-list.component.ts
+++ src/app/pages/category/category-list/category-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CategoryService } from '../shared/category.service'
+import { IrequestCategory } from '../shared/category.model'
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
@@ -7,13 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http:CategoryService) { }
   
+  categories:IrequestCategory[]=[]
+
   ngOnInit(): void {
+    this.getCategories()
   }
 
   alert(value){
     alert(value)
   }
+  
+   getCategories(){
+     this.http.getAll()
+          .subscribe(
+            res=>{
+              this.categories=res
+            }
+          )
+   }
 
+   delete=({ id })=>{
+     const confirmDelete=confirm("desejas deletar!")
+      if(confirmDelete){
+      this.http.delete(id).subscribe(
+        ()=>{
+          this.categories=this.categories.filter(data=>data.id!=id)
+        },
+        ()=>alert("Erro ao tentar deletar!")
+      )
+    }
+   }
 }
